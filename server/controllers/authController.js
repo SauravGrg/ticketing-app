0f6cb1564139da278b0207e84541df53710dda65
Saurav Gurung <sauravgurung115@gmail.com>
2025-12-12 12:10:05 -0500

    Implement user login endpoint and JWT generation

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,12 +1,16 @@
 const bcrypt = require("bcrypt");
 const { User } = require("../models");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
     console.log("Received: ", { name, email, password, role });
 
-    const emailExist = await User.findOne({ where: { email: email } });
+    const emailExist = await User.findOne({
+      where: { email: email },
+      attributes: ["email"],
+    });
     if (emailExist) {
       return res.status(409).json({
         success: false,
@@ -40,4 +44,44 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  const { name, email, password, role } = req.body;
+  const existingUser = await User.findOne({
+    where: { email: email },
+  });
+  if (!existingUser) {
+    return res.status(401).json({
+      success: false,
+      message: "User doesn't exist",
+    });
+  } else {
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Wrong Password",
+      });
+    } else {
+      const payload = {
+        userId: existingUser.id,
+        role: existingUser.role,
+      };
+      const secretKey = process.env.JWT_SECRET;
+      const token = jwt.sign(payload, secretKey, { expiresIn: "1h" });
+
+      res.status(200).json({
+        success: true,
+        message: "Login successfully",
+        token: token,
+        user: {
+          id: existingUser.id,
+          name: existingUser.name,
+          email: existingUser.email,
+          role: existingUser.role,
+        },
+      });
+    }
+  }
+};
+
+module.exports = { register, login };
